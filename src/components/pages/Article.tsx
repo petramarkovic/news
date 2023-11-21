b837c4e5e96904729bf802d91938717de67320a6
Petra Markovic <petra.markovic@vegait.rs>
2023-11-21 13:19:41 +0100

    Add styles to go back link

--- src/components/pages/Article.tsx
+++ src/components/pages/Article.tsx
@@ -1,22 +1,25 @@
-import * as reactRouterDom from 'react-router-dom'
-import { Wrap } from '../ui/Wrap'
+import * as reactRouterDom from "react-router-dom";
+import { Wrap } from "../ui/Wrap";
+import { Link } from "react-router-dom";
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 
 export const Article = () => {
-	const { state } = reactRouterDom.useLocation()
-	const articleData = state
+  const { state } = reactRouterDom.useLocation();
+  const articleData = state;
 
-	return (
-		<div className=''>
-			<Wrap>
-				<article>
-					<h1>{articleData.title}</h1>
-					<img
-						src={articleData.urlToImage}
-						alt={articleData.description}
-					/>
-					<p>{articleData.content}</p>
-				</article>
-			</Wrap>
-		</div>
-	)
-}
+  return (
+    <div className="">
+      <Wrap>
+        <article>
+          <h1>{articleData.title}</h1>
+          <img src={articleData.urlToImage} alt={articleData.description} />
+          <p>{articleData.content}</p>
+        </article>
+        <Link to="/" className="link link--purple inline-flex items-center">
+          <ChevronLeftIcon className="h-6 w-5 hover:stroke-violet-500 mr-2" />
+          Go back to the list
+        </Link>
+      </Wrap>
+    </div>
+  );
+};
