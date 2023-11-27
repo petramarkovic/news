d08aa4fa121b98a26fa99dba8ce54325e149c0ae
petramarkovic <petramarkovic996@gmail.com>
2023-11-27 22:14:39 +0100

    Design change in progress

--- src/components/containers/Navbar.tsx
+++ src/components/containers/Navbar.tsx
@@ -1,26 +1,22 @@
-import { Link } from 'react-router-dom'
-import { Button } from '../ui/Button'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { Link } from "react-router-dom";
+import { Button } from "../ui/Button";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export const Navbar = () => {
-	return (
-		<nav>
-			<ul className='flex'>
-				<Link
-					to='/'
-					className='link'>
-					Top News
-				</Link>
-				<Link
-					to='/categories'
-					className='link'>
-					Categories
-				</Link>
-				<Button className='link'>
-					Search
-					<MagnifyingGlassIcon className='h-6 w-6 ml-2 tex-violet-600 inline' />
-				</Button>
-			</ul>
-		</nav>
-	)
-}
+  return (
+    <nav className="ml-auto">
+      <ul className="flex">
+        <Link to="/" className="link">
+          Top News
+        </Link>
+        <Link to="/categories" className="link">
+          Categories
+        </Link>
+        <Button className="link">
+          Search
+          <MagnifyingGlassIcon className="h-6 w-6 ml-2 tex-violet-600 inline" />
+        </Button>
+      </ul>
+    </nav>
+  );
+};
