4d240a3dd440491845936effab4468a22712af01
petramarkovic <petramarkovic996@gmail.com>
2023-11-27 21:51:10 +0100

    Add categories page

---
+++ src/components/containers/Card.tsx
@@ -0,0 +1,66 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+
+type ArticleType = {
+  title: string;
+  urlToImage: string;
+  description: string;
+};
+
+interface CardProps {
+  data: {
+    articles: ArticleType[];
+  };
+}
+
+export const Card = ({ data }: CardProps) => {
+  const removedArticle = "Removed";
+  const maxArticlesToShow = 5;
+
+  const breakpoints = {
+    400: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  };
+
+  return (
+    <Swiper
+      breakpoints={breakpoints}
+      slidesPerView={3}
+      spaceBetween={30}
+      navigation
+      modules={[Navigation]}
+      className="py-6"
+    >
+      {data.articles
+        .filter((article) => !article.title.includes(removedArticle))
+        .slice(0, maxArticlesToShow)
+        .map((article, index) => (
+          <SwiperSlide
+            key={index}
+            className="self-stretch h-auto flex flex-col"
+          >
+            <h3 className="mb-4 text-black font-bold">{article.title}</h3>
+            <div className="relative pb-56-25 mt-auto">
+              <img
+                src={article.urlToImage ?? "placeholder-image.jpg"}
+                alt={article.description}
+                className="absolute top-0 left-0 w-full h-full object-cover"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+    </Swiper>
+  );
+};
--- src/components/pages/Categories.tsx
+++ src/components/pages/Categories.tsx
@@ -1,5 +1,64 @@
 import { Wrap } from "../ui/Wrap";
+import { useLanguageContext } from "../../store/languageContext";
+import { useState, useEffect } from "react";
+import { Category } from "../containers/Category";
+
+type MyDataType = {
+  articles: {
+    title: string;
+    content: string;
+    author: string;
+    urlToImage: string;
+    description: string;
+  }[];
+};
 
 export const Categories = () => {
-  return <div>Categories</div>;
+  const { lang } = useLanguageContext();
+  const key = `${process.env.REACT_APP_API_KEY}`;
+  const categories = [
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology",
+  ];
+
+  const [categoryData, setCategoryData] = useState<MyDataType[]>([]);
+
+  useEffect(() => {
+    const fetchDataForCategories = async () => {
+      const dataForCategories: MyDataType[] = [];
+
+      for (const category of categories) {
+        const response = await fetch(
+          `https://newsapi.org/v2/top-headlines?country=${lang}&category=${category}&apiKey=${key}`
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          dataForCategories.push(data);
+        }
+      }
+
+      setCategoryData(dataForCategories);
+    };
+
+    fetchDataForCategories();
+  }, [lang]);
+
+  return (
+    <div>
+      <Wrap>
+        <h1 className="font-extrabold p-2 my-8 text-5xl text-slate-900">
+          Top 5 news by categories in {lang}:
+        </h1>
+        {categoryData.map((data, index) => (
+          <Category data={data} key={index} title={categories[index]} />
+        ))}
+      </Wrap>
+    </div>
+  );
 };
