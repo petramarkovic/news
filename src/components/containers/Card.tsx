2538032a3608598d2b3dc3eb0aeb2907b208a5fc
Petra Markovic <petra.markovic@vegait.rs>
2023-12-07 13:27:52 +0100

    Fix image path

--- src/components/containers/Card.tsx
+++ src/components/containers/Card.tsx
@@ -7,7 +7,7 @@ export const Card: React.FC<ArticleInterface> = (props) => {
       <h3 className="mb-4 text-neutral-200 font-normal">{props.title}</h3>
       <div className="relative pb-56-25 mt-auto">
         <img
-          src={props.urlToImage ?? "placeholder-image.jpg"}
+          src={props.urlToImage ?? "../placeholder-image.jpg"}
           alt={props.description}
           className="absolute top-0 left-0 w-full h-full object-cover rounded-lg opacity-60"
         />
