55058929dc54f40ed1d807539a4a406966bb76b7
Petra Markovic <petra.markovic@vegait.rs>
2024-01-09 10:31:52 +0100

    Fetch category and show skeleton within each category

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,12 +1,9 @@
 import { useLanguageContext } from "../store/languageContext";
-import { useState, useEffect } from "react";
-import { ArticlesArrayInterface } from "../types";
 import { Wrap } from "../components/UI/Wrap";
-import { Category, CategorySkeleton } from "../components/Category";
+import { Category } from "../components/Category";
 
 export const Categories = () => {
   const { lang } = useLanguageContext();
-  const key = `${process.env.REACT_APP_API_KEY}`;
   const categories = [
     "business",
     "entertainment",
@@ -17,50 +14,14 @@ export const Categories = () => {
     "technology",
   ];
 
-  const [categoryData, setCategoryData] = useState<ArticlesArrayInterface[]>(
-    []
-  );
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchDataForCategories = async () => {
-      const dataForCategories: ArticlesArrayInterface[] = [];
-
-      for (const category of categories) {
-        const response = await fetch(
-          `https://newsapi.org/v2/top-headlines?country=${lang}&category=${category}&apiKey=${key}`
-        );
-
-        if (response.ok) {
-          const data = await response.json();
-          dataForCategories.push(data);
-        }
-      }
-
-      setCategoryData(dataForCategories);
-      setIsLoading(false);
-    };
-
-    fetchDataForCategories();
-  }, [lang]);
-
-  const skeletonArray = Array.from({ length: 7 }, (_, index) => (
-    <CategorySkeleton key={index} />
-  ));
-
   return (
     <div className="bg-stone-950 py-20 min-h-screen">
       <Wrap>
         <h1 className="p-2 mb-8 text-4xl text-neutral-50 border-b-2 border-rose-400 border-opacity-10">
           Top 5 news by categories in {lang}:
         </h1>
-        {isLoading && skeletonArray}
-        {categoryData.map((data, index) => (
-          <Category
-            articles={data.articles}
-            key={index}
-            title={categories[index]}
-          />
+        {categories.map((category, index) => (
+          <Category category={category} key={index} />
         ))}
       </Wrap>
     </div>
