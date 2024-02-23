5e8de4fc53a37bf677dae1a852a736fdc0800ba3
Petra Markovic <petra.markovic@vegait.rs>
2024-02-23 09:58:43 +0100

    Remove try/catch block from fetchData

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,15 +1,9 @@
 export const fetchData = async <T>(url: string): Promise<T> => {
-	try {
-		const res = await fetch(url);
-		if (!res.ok) {
-			throw new Error('Error fetching data');
-		}
-
-		const data: T = await res.json();
-		return data;
-	} catch (error: any) {
-		// TODO Throwing error inside of a catch is wrong.
-		// Since you use useQuery wrapper, you can omit try/catch and just throw error if response is not ok as you are already doing
-		throw new Error(error?.message);
+	const res = await fetch(url);
+	if (!res.ok) {
+		throw new Error('Error fetching data');
 	}
+
+	const data: T = await res.json();
+	return data;
 };
